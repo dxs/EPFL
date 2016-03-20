d3a1a75311e31c2ff9b2b961cb30e30e165303ec
dxs <sborden@bluewin.ch>
2016-03-20 15:51:09 +0100

    fiat

--- HACKERRANK_TOOLS.cs
+++ HACKERRANK_TOOLS.cs
@@ -1,6 +1,10 @@
-INPUT PARSING
+//INPUT PARSING
 
 int n = Convert.ToInt32(Console.ReadLine());
 
 string[] tmp = Console.ReadLine().Split(' ');
 int[] arr = Attay.ConvertAll(tmp, Int32.Parse);
+
+int a;
+
+var a = 5;
